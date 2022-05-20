fe674a6b090f75d98c30eee4b6b54dd16d255f38
vitaliimkb <vitalii.movchan.mkb.2019@lpnu.ua>
2022-05-20 18:00:03 +0300

    Add new route

--- app.py
+++ app.py
@@ -14,5 +14,10 @@ def about():
     return '<h3>About us!</h3>'
 
 
+@app.route('/contacts') # прописується роут(шлях) адреса
+def about():
+    return '<h3>Contacts</h3>'
+
+
 if __name__ == '__main__':
     app.run(debug=True)
