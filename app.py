0a32fda34c4c34e02e7566f87d0ad42df4d193a3
vitaliimkb <vitalii.movchan.mkb.2019@lpnu.ua>
2022-05-20 17:38:26 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+
+app = Flask(__name__)  # створюється об'єкт фласка в ДАНОМУ файлі
+
+
+@app.route('/') # прописується роут(шлях) адреса
+def hello_world():
+    return '<h1><i>Hello World!</i></h1>'
+# i(italic), b(bold), u(underline), s
+
+
+@app.route('/about') # прописується роут(шлях) адреса
+def about():
+    return '<h3>About us!</h3>'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
