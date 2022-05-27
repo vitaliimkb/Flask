5ec85ef34379674f5ea36a8511d14a7301c1813d
vitaliimkb <vitalii.movchan.mkb.2019@lpnu.ua>
2022-05-27 17:42:45 +0300

    Add jinja and render templates

--- app.py
+++ app.py
@@ -1,22 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
-app = Flask(__name__)  # створюється об'єкт фласка в ДАНОМУ файлі
+app = Flask(__name__)
 
 
-@app.route('/') # прописується роут(шлях) адреса
+@app.route('/')
 def hello_world():
-    return '<h1><i>Hello World!</i></h1>'
-# i(italic), b(bold), u(underline), s
+    is_login = False
+    array = ["task1", "task2", "task3", "task4"]
+    return render_template("index.html", object={"name": "Vitalii", "age": 24},
+                           array=array, is_login=is_login)
 
 
-@app.route('/about') # прописується роут(шлях) адреса
+@app.route('/about', methods=['POST', 'GET'])
 def about():
-    return '<h3>About us!</h3>'
+    if request.method == "POST":
+        title = request.form["title"]
+        return render_template("about.html", title=title)
+    elif request.method == "GET":
+        title = request.args.get("title")
+        return render_template("about.html", title=title)
 
-
-@app.route('/contacts') # прописується роут(шлях) адреса
-def about():
-    return '<h3>Contacts</h3>'
+    return render_template("about.html")
 
 
 if __name__ == '__main__':
