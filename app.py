962d6d92a2e59e6d7350644106bb3a25d7b39bbb
movcan77 <movcan77@gmail.com>
2022-06-10 16:39:18 +0300

    Add function for adding artist

--- app.py
+++ app.py
@@ -1,26 +1,37 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
+import sqlite3
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    is_login = False
-    array = ["task1", "task2", "task3", "task4"]
-    return render_template("index.html", object={"name": "Vitalii", "age": 24},
-                           array=array, is_login=is_login)
+    pass
 
 
-@app.route('/about', methods=['POST', 'GET'])
-def about():
+@app.route("/add-artist/", methods=["GET", "POST"])
+def add_artist():
     if request.method == "POST":
-        title = request.form["title"]
-        return render_template("about.html", title=title)
-    elif request.method == "GET":
-        title = request.args.get("title")
-        return render_template("about.html", title=title)
+        name_artist = request.form["name"]
+        print(name_artist)
+        execute(f"INSERT INTO artist (name) VALUES ('{name_artist}')")
+        return redirect("/artists")
+    else:
+        return render_template("add-artist.html")
 
-    return render_template("about.html")
+
+@app.route("/artists/")
+def get_artists():
+    artists = execute("SELECT * FROM artist").fetchall()
+    return render_template("artists.html", artists=artists)
+
+
+def execute(query):
+    with sqlite3.connect("music.db") as conn:
+        cursor = conn.cursor()
+        cursor.execute(query)
+        conn.commit()
+        return cursor
 
 
 if __name__ == '__main__':
