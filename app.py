f4ac5fd1b3a038ecad0352c9e8d92b47181c080f
Vitaliy Movchan <movcan77@gmail.com>
2022-06-27 14:57:33 +0300

    Add edit song, search, song by user and song_detail

--- app.py
+++ app.py
@@ -6,7 +6,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return render_template("index.html", heading="Main")
+    context = dict()
+    songs = execute(f"""
+                SELECT song.id, song.name, artist.name FROM song 
+                INNER JOIN artist ON song.artist_id = artist.id
+            """).fetchall()
+    artists = execute(f"""
+                SELECT * FROM artist
+        """).fetchall()
+
+    context['songs'] = songs
+    context['artists'] = artists
+    context['heading'] = "Main"
+    return render_template("index.html", **context)
 
 
 # ------------------------Artists----------------------------------------
@@ -114,6 +126,23 @@ def song_delete(pk):
     return redirect("/songs")
 
 
+@app.route("/songs/<int:pk>/edit", methods=["GET", "POST"])
+def edit_track(pk):
+    if request.method == "POST":
+        update_full_song(pk)
+        return redirect("/")
+    else:
+        song = execute(f"""SELECT a.id, a.name AS artist, s.name AS song
+                            FROM song AS s
+                            INNER JOIN artist a ON s.artist_id = a.id
+                            WHERE s.id = {pk}""").fetchone()
+
+        genres = execute(f"""SELECT id, name FROM song_genre 
+        INNER JOIN genre g on g.id = song_genre.genre_id 
+        WHERE song_id={pk}""").fetchall()
+        return render_template("edit-song.html", song=song, **get_artists_and_genres(), current_genres=genres)
+
+
 def insert_full_song():
     title = request.form['title']
     artist = request.form['artist']
@@ -124,6 +153,16 @@ def insert_full_song():
         execute(f"INSERT INTO song_genre (song_id, genre_id) VALUES {last_song_id, genre}")
 
 
+def update_full_song(pk):
+    title = request.form['title']
+    artist = request.form['artist']
+    genres = request.form.getlist('genre')
+    execute(f"UPDATE song SET name='{title}', artist_id={artist} WHERE id={pk}")
+    execute(f"DELETE FROM song_genre WHERE song_id={pk}")
+    for genre in genres:
+        execute(f"INSERT INTO song_genre (song_id, genre_id) VALUES {pk, genre}")
+
+
 def get_artists_and_genres():
     context = dict()
     artists = execute("SELECT * FROM artist ORDER BY name")
@@ -132,8 +171,52 @@ def get_artists_and_genres():
     context['genres'] = genres
     return context
 
+
 # ------------------------Songs----------------------------------------
 
+
+# ------------------------Other----------------------------------------
+
+@app.route("/search")
+def search():
+    context = dict()
+    query = request.values['query']
+    songs = execute(f"""
+            SELECT song.id, song.name, artist.name FROM song 
+            INNER JOIN artist ON song.artist_id = artist.id
+            WHERE song.name LIKE '%{query}%'
+        """).fetchall()
+    artists = execute(f"""
+            SELECT * FROM artist WHERE artist.name LIKE '%{query}%'
+    """).fetchall()
+
+    context['songs'] = songs
+    context['artists'] = artists
+    context['heading'] = "Search results"
+    return render_template("index.html", **context)
+
+
+@app.route("/songs/<int:pk>/detail")
+def song_detail(pk):
+    song = execute(f"""
+        SELECT s.id, a.name AS artist, s.name AS song_name, GROUP_CONCAT(g.name) AS genre
+        FROM song as s 
+        INNER JOIN song_genre sg on s.id = sg.song_id
+        INNER JOIN genre g on g.id = sg.genre_id
+        INNER JOIN artist a on s.artist_id = a.id
+        WHERE s.id = {pk}
+    """).fetchone()
+    return render_template("song-detail.html", song=song)
+
+
+@app.route("/artists/<int:pk>/songs")
+def songs_by_artist(pk):
+    songs = execute(f"""
+                SELECT song.id, song.name, artist.name FROM song 
+                INNER JOIN artist ON song.artist_id = artist.id
+                WHERE artist_id = {pk}
+            """).fetchall()
+    return render_template("index.html", songs=songs)
 
 def execute(query):
     with sqlite3.connect("C:/Users/Vitalii/PycharmProjects/Flask/music.db") as conn:
@@ -142,6 +225,10 @@ def execute(query):
         conn.commit()
         return cursor
 
+# song edit
+# song by artist
+
 
 if __name__ == '__main__':
     app.run(debug=True)
+# ------------------------Other----------------------------------------
